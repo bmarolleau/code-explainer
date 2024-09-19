d37116d53a89494f387ede9e6fb607bc36ade4f8
Benoit Marolleau <benoit.marolleau@fr.ibm.com>
2024-09-19 14:39:47 +0200

    commenting backend code

--- backend/main.py
+++ backend/main.py
@@ -45,45 +45,37 @@ async def generate_code(request_data: CodeGenerationRequest):
 
 @app.post("/v1/wca4z-explain")
 async def generate_code2(request_data: CodeExplainWCAZ):
+    #************************************** 
+    # Get Bearer Token generation from API Key:
+    #**************************************
     url = "https://iam.cloud.ibm.com/identity/token"
-
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-       # 'Authorization': f'Bearer {request_data.api_key}',
+        'Content-Type': 'application/x-www-form-urlencoded',     
     }
-
-    print(request_data.model_dump_json())
-
-
-    request_data2="grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey="+request_data.api_key
-    #request_data.__delattr__('api_key')
-    #print("HEADERS: "+ headers.__str__)
     #print(request_data.model_dump_json())
-
+    request_data2="grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey="+request_data.api_key
     response = requests.request("POST", url, headers=headers, data=request_data2)
-    #print(response.json())
     json_data = response.json()
-   # json=json.dumps(response.json(), indent=4)
     token=json_data['access_token']
-   # print("TOKEN="+token)
-
+    #***************************************************************** 
+    # Invoke WCAZ Explain API:  input= Level, base64 code, bearer token
+    #*****************************************************************
+    # First lets extract the input Level: SIMPLE, DETAILED, GUIDED
+    # print("bearer token="+token)
     level=request_data.level
-    print("level="+level)
-
     url = "https://api.dataplatform.cloud.ibm.com/v1/wca/code/explanation/COBOL?level="+level
-
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {token}',
         'user-agent' : 'zCodeAssistant/2.1.0' 
     }
+    #Remove API Key and Level passed in the POST data part of the initial request 
     request_data.__delattr__('api_key')
     request_data.__delattr__('level')
     response = requests.request("POST", url, headers=headers, data=request_data.model_dump_json())
     json_data = response.json()
     print(response.json())
     code=json_data['generated_text']
-
     return code
 
 
